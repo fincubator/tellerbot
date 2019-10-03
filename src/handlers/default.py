21e782931da5870ad13b8e75bf408ca2ec82f8e8
alfred richardsn <rchrdsn@protonmail.ch>
2019-10-03 20:19:57 +0300

    Add default callback query handler
    Signed-off-by: alfred richardsn <rchrdsn@protonmail.ch>

--- src/handlers/default.py
+++ src/handlers/default.py
@@ -16,15 +16,21 @@
 # along with TellerBot.  If not, see <https://www.gnu.org/licenses/>.
 
 
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
+from aiogram.dispatcher.filters.state import any_state
 
-from . import tg, private_handler, start_keyboard
+from . import tg, dp, private_handler, start_keyboard
 from ..i18n import _
 
 
-@private_handler()
-async def default(message: Message):
+@private_handler(state=any_state)
+async def default_message(message: Message):
     await tg.send_message(
         message.chat.id, _('Unknown command.'),
         reply_markup=start_keyboard()
     )
+
+
+@dp.callback_query_handler(state=any_state)
+async def default_callback_query(call: CallbackQuery):
+    await call.answer(_('Unknown button.'))
