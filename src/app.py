a9b1021452d4ed6a6e002effd5c7b8cd036af8f8
alfred richardsn <rchrdsn@protonmail.ch>
2019-11-12 21:34:21 +0300

    Concatenate random token to webhook path
    Signed-off-by: alfred richardsn <rchrdsn@protonmail.ch>

--- src/app.py
+++ src/app.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with TellerBot.  If not, see <https://www.gnu.org/licenses/>.
 import asyncio
+import secrets
 
 from aiogram.utils import executor
 
@@ -25,14 +26,13 @@ from src.registered_handlers import dp
 from src.registered_handlers import tg
 
 
-async def on_startup(*args):
+async def on_startup(webhook_path, *args):
     """Prepare bot before starting.
 
     Set webhook and run background tasks.
     """
     await tg.delete_webhook()
-    url = 'https://{}'.format(config.SERVER_HOST)
-    await tg.set_webhook(url + config.WEBHOOK_PATH)
+    await tg.set_webhook('https://' + config.SERVER_HOST + webhook_path)
     asyncio.create_task(notifications.run_loop())
     asyncio.create_task(connect_to_blockchains())
 
@@ -42,10 +42,13 @@ def main():
 
     Bot's main entry point.
     """
+    url_token = secrets.token_urlsafe()
+    webhook_path = config.WEBHOOK_PATH + '/' + url_token
+
     executor.start_webhook(
         dispatcher=dp,
-        webhook_path=config.WEBHOOK_PATH,
-        on_startup=on_startup,
+        webhook_path=webhook_path,
+        on_startup=lambda *args: on_startup(webhook_path, *args),
         host='127.0.0.1',
         port=config.SERVER_PORT,
     )
