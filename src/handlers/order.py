5e4789df0b457b168b24842b4d62e96638f69912
retsuchisei <misternort@gmail.com>
2019-12-05 03:45:54 +0300

    Added commands /s and /creator
    Signed-off-by: retsuchisei <misternort@gmail.com>

--- src/handlers/order.py
+++ src/handlers/order.py
@@ -46,7 +46,6 @@ from src.money import money
 from src.money import MoneyValueError
 from src.money import normalize
 
-
 OrderType = typing.Mapping[str, typing.Any]
 
 
@@ -637,3 +636,87 @@ async def hide_button(call: types.CallbackQuery):
     location_message_id = call.data.split()[1]
     if location_message_id != "-1":
         await tg.delete_message(call.message.chat.id, location_message_id)
+
+
+@private_handler(commands=["s"], state=any_state)
+async def choose_currencies(message: types.Message, state: FSMContext):
+    """
+    Search orders by currencies.
+
+    When a user enter /s.
+    {str1} {str2} |str1 is sell value, str2 is buy value.
+      *    {str2} |sell accepts any value, buy is str2.
+    {str1}   *    |*^^^^*
+    {str1}        |str is sell value, but accepts any value.
+    """
+    query = {
+        "$and": [
+            {
+                "$or": [
+                    {"expiration_time": {"$exists": False}},
+                    {"expiration_time": {"$gt": time()}},
+                ]
+            },
+        ]
+    }
+
+    source = message.text.upper().split()
+    try:
+        sell, buy = source[1], source[2]
+        if sell != "*":
+            query["sell"] = sell
+        if buy != "*":
+            query["buy"] = buy
+    except IndexError:
+        if len(source) == 1:
+            await tg.send_message(
+                message.chat.id, _("You have not entered a currency.")
+            )
+            return
+        else:
+            query["$and"] += [{"$or": [{"sell": source[1]}, {"buy": source[1]}]}]
+            print(query)
+
+    cursor = database.orders.find(query).sort("start_time", pymongo.DESCENDING)
+    quantity = await database.orders.count_documents(query)
+    await state.finish()
+    await orders_list(
+        cursor, message.chat.id, 0, quantity, "orders", user_id=message.from_user.id
+    )
+
+
+@private_handler(commands=["creator"], state=any_state)
+async def search_by_creator(message: types.Message, state: FSMContext):
+    """
+    User enter /creator {username}.
+
+    The function returns orders of the given person.
+    """
+    source = message.text.split()
+    try:
+        creator = source[1]
+        if creator[0] != "@":
+            await tg.send_message(
+                message.chat.id, _("You did not enter username."),
+            )
+            return
+
+    except IndexError:
+        await tg.send_message(
+            message.chat.id, _("You did not enter username."),
+        )
+        return
+
+    query = {
+        "mention": creator,
+        "$or": [
+            {"expiration_time": {"$exists": False}},
+            {"expiration_time": {"$gt": time()}},
+        ],
+    }
+    cursor = database.orders.find(query).sort("start_time", pymongo.DESCENDING)
+    quantity = await database.orders.count_documents(query)
+    await state.finish()
+    await orders_list(
+        cursor, message.chat.id, 0, quantity, "orders", user_id=message.from_user.id
+    )
