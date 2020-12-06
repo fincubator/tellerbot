d29a0aa92163f4d5d2353d86ac9d3a91d8e4b0eb
alfred richardsn <rchrdsn@protonmail.ch>
2020-12-06 23:02:39 +0300

    Strip trailing characters in database password
    Signed-off-by: alfred richardsn <rchrdsn@protonmail.ch>

--- mongo-init.sh
+++ mongo-init.sh
@@ -3,7 +3,7 @@ mongo -- "${MONGO_INITDB_DATABASE-$DATABASE_NAME}" <<EOF
 db.createUser(
     {
         user: "${MONGO_INITDB_ROOT_USERNAME:-$DATABASE_USERNAME}",
-        pwd: "${MONGO_INITDB_ROOT_PASSWORD:-$(cat "${DATABASE_PASSWORD_FILENAME}")}",
+        pwd: "$(printf %s "${MONGO_INITDB_ROOT_PASSWORD:-$(cat ${DATABASE_PASSWORD_FILENAME})}")",
         roles: ["readWrite"]
     }
 );
